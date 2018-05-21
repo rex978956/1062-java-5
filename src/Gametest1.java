c7d444c43b3e310269319de9854462b59adef2ec
rex978956 <rex978956@gmail.com>
2018-05-22 01:50:40 +0800

    add GameState3 for slidertest

--- src/Gametest1.java
+++ src/Gametest1.java
@@ -14,13 +14,12 @@ public class Gametest1 extends StateBasedGame {
 
     @Override
     public void initStatesList(GameContainer gameContainer) throws SlickException {
-        this.addState(new GameState());
-        this.addState(new GameState2());
+        this.addState(new GameState3());
     }
 
     public static void main(String[] args) throws SlickException {
         AppGameContainer app = new AppGameContainer(new Gametest1("Gametest1"));
-        app.setDisplayMode(800, 600, false);        //app.setVSync(true); //set FPS 同步 (多線程) 好像預設60
+        app.setDisplayMode(800 ,600,false);        //app.setVSync(true); //set FPS 同步 (多線程) 好像預設60
         app.setTargetFrameRate(60); //set FPS
         app.setAlwaysRender(true);
         app.start();
