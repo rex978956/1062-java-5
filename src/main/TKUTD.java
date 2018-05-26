bec7e37a8b9a1177c130e8a45018141000c3294b
allen0099 <s96016641@gmail.com>
2018-05-26 14:29:07 +0800

    Add full screen support

--- src/main/TKUTD.java
+++ src/main/TKUTD.java
@@ -1,19 +1,22 @@
 package main;
 
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.StateBasedGame;
 import states.MainMenu;
 import states.MapMenu;
 
 public class TKUTD extends StateBasedGame {
 
+    private static final int WINDOWED_WIDTH = 1280;
+    private static final int WINDOWED_HEIGHT = 800;
+    private AppGameContainer container;
+
     private TKUTD(String name) {
         super(name);
     }
 
     public void initStatesList(GameContainer gc) {
+        this.container = (AppGameContainer) gc;
         addState(new MainMenu());
         addState(new MapMenu());
     }
@@ -30,6 +33,27 @@ public class TKUTD extends StateBasedGame {
             game.start();
         } catch (SlickException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param key
+     * @param c
+     * @see InputListener#keyPressed(int, char)
+     */
+    @Override
+    public void keyPressed(int key, char c) {
+        super.keyPressed(key, c);
+        if (key == Input.KEY_F11) {
+            try {
+                if (container.isFullscreen()) {
+                    container.setDisplayMode(WINDOWED_WIDTH, WINDOWED_HEIGHT, false);
+                } else {
+                    container.setDisplayMode(container.getScreenWidth(), container.getScreenHeight(), true);
+                }
+            } catch (SlickException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
