e2b27896804b33a6cdf6824b15fdfdd1c4f9c390
Bandit <rex978956@gmail.com>
2018-05-26 18:37:11 +0800

    add Fonts

---
+++ src/states/FontDemo.java
@@ -0,0 +1,35 @@
+package states;
+
+import misc.FontSet;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class FontDemo extends BasicGameState {
+
+    @Override
+    public void init(GameContainer gameContainer, StateBasedGame stateBasedGame) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, StateBasedGame stateBasedGame, Graphics graphics) throws SlickException {
+        FontSet.draw("Demo58",550,100, 58);
+        FontSet.draw("Demo50",550,200, 50);
+        FontSet.draw("Demo42",550,300, 42);
+        FontSet.draw("Demo34",550,400, 34);
+        FontSet.draw("Demo16",550,500, 26);
+        FontSet.draw("Demo18",550,600, 18);
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, StateBasedGame stateBasedGame, int i) throws SlickException {
+
+    }
+
+    public int getID() {
+        return 99;
+    }
+}
